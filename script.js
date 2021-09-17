59ce7871a2068c656c5c336bfaa7a64dce86ebf1
mohammadyousaf0 <abdul2015@outlook.dk>
2021-09-17 12:07:13 +0200

    add airbnb solution

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-let wrapperElement = document.querySelector("#team");
+let wrapperElement = document.querySelector(".scrollhouse");
 
-fetch("./teamdata.json")
+fetch("./huse.json")
 .then((response) => response.json())
 .then((result) => {
-    result.teamdata.forEach((teamMember) => {
+    result.huse.forEach((huseNumber) => {
         wrapperElement.innerHTML += `
-        <section id="team">
-        <p class="firstName">${teamMember.firstName}</p>
-      </section>`
+        <div class="flexhouseimgtext">
+
+          <div class="hus">
+            <a href="hus.html"><img src="${huseNumber.img}" alt="hus"></a>
+          </div>
+
+          <div class="hustext">
+            <div class="smalltext">
+              <p>${huseNumber.smallText}</p>
+              <i class="far fa-heart"></i>
+            </div>
+              <p class="vej"><a href="hus.html">${huseNumber.roadName}</a></p>
+              <p class="guest">${huseNumber.guests}</p>
+          
+            <div class="review">
+                <i class="fas fa-star"></i>
+                <p>${huseNumber.reviews}</p>               
+            </div>
+          </div>
+      </div>`
     })
 })
